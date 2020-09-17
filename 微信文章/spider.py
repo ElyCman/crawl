e85d47f91bd79e5d3d32296b4cc06a8bea7f479e
ElyCman <35475879+ElyCman@users.noreply.github.com>
2020-09-17 08:57:48 +0800

    Update spider.py

--- 微信文章/spider.py
+++ 微信文章/spider.py
@@ -17,7 +17,7 @@ base_url = 'https://weixin.sogou.com/weixin?'
 # get proxy from proxy pool
 def get_proxy():
     try:
-        response = requests.get(PROXY_POOL_URL)
+        response = requests.get(PROXY_POOL_URL)  # your proxy pool
         if response.status_code == 200:
             proxy = response.text
             proxy_pool = {
@@ -49,7 +49,8 @@ def get_index(url, proxy):
         'Accept-Encoding': 'gzip, deflate, br',
         'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
         'Connection': 'keep-alive',
-        'Cookie': 'ABTEST=8|1594265040|v1; SUID=2A0149DF4A42910A000000005F068DD0; SUID=2A0149DF3320910A000000005F068DD1; weixinIndexVisited=1; SUV=009D075CDF49012A5F068DD1C1092547; SNUID=EFC48C1AC5C06C3F076CC4E3C6D40196; ppinf=5|1594265681|1595475281|dHJ1c3Q6MToxfGNsaWVudGlkOjQ6MjAxN3x1bmlxbmFtZToyNzolRTclQTUlOUUlRTclQkIlOEYlRTglOUIlOTl8Y3J0OjEwOjE1OTQyNjU2ODF8cmVmbmljazoyNzolRTclQTUlOUUlRTclQkIlOEYlRTglOUIlOTl8dXNlcmlkOjQ0Om85dDJsdUZRcGNPN2NYaVlvR2hJTXJmOUNGV3dAd2VpeGluLnNvaHUuY29tfA; pprdig=Jo6FKkw9vA8XF-BZW6ZmiMWEVkEPKggTv1_-FH6K3WQLfvx5ErXBbrXr1ClyRQwv1IFNmtKrUWdIzXIPj95zx98EYfxTN5QJbfDb4xmxg6WDABpM3rT8WGZcKtlnRlCz7QbzzckNuefKx35oIv16A537mcH9q5hGXbUBJSC5_w8; sgid=18-48709535-AV8GkFEk9fqkufJhtfZr0B4; ppmdig=15942722750000007fbefb3d6255d76c93a83267abe34bb1; JSESSIONID=aaaGJ7BnSIo-15uiK0omx',
+        # your cookie
+        'Cookie': 'ABTEST=******; SUID=******; SUID=******; weixinIndexVisited=******; SUV=******; SNUID=******; ppinf=******; pprdig=******; sgid=******; ppmdig=******; JSESSIONID=******',
         'Host': 'weixin.sogou.com',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
@@ -83,7 +84,8 @@ def get_real_link(index_url, k_h_url, proxy):
         'Accept-Encoding': 'gzip, deflate, br',
         'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
         'Connection': 'keep-alive',
-        'Cookie': 'ABTEST=8|1594265040|v1; SUID=2A0149DF4A42910A000000005F068DD0; SUID=2A0149DF3320910A000000005F068DD1; weixinIndexVisited=1; SUV=009D075CDF49012A5F068DD1C1092547; SNUID=EFC48C1AC5C06C3F076CC4E3C6D40196; ppinf=5|1594265681|1595475281|dHJ1c3Q6MToxfGNsaWVudGlkOjQ6MjAxN3x1bmlxbmFtZToyNzolRTclQTUlOUUlRTclQkIlOEYlRTglOUIlOTl8Y3J0OjEwOjE1OTQyNjU2ODF8cmVmbmljazoyNzolRTclQTUlOUUlRTclQkIlOEYlRTglOUIlOTl8dXNlcmlkOjQ0Om85dDJsdUZRcGNPN2NYaVlvR2hJTXJmOUNGV3dAd2VpeGluLnNvaHUuY29tfA; pprdig=Jo6FKkw9vA8XF-BZW6ZmiMWEVkEPKggTv1_-FH6K3WQLfvx5ErXBbrXr1ClyRQwv1IFNmtKrUWdIzXIPj95zx98EYfxTN5QJbfDb4xmxg6WDABpM3rT8WGZcKtlnRlCz7QbzzckNuefKx35oIv16A537mcH9q5hGXbUBJSC5_w8; sgid=18-48709535-AV8GkFEk9fqkufJhtfZr0B4; ppmdig=15942722750000007fbefb3d6255d76c93a83267abe34bb1; JSESSIONID=aaaGJ7BnSIo-15uiK0omx',
+        # your cookie
+        'Cookie': 'ABTEST=******; SUID=******; SUID=******; weixinIndexVisited=******; SUV=******; SNUID=******; ppinf=******; pprdig=******; sgid=******; ppmdig=******; JSESSIONID=******',
         'Host': 'weixin.sogou.com',
         'Refere': index_url,
         'Upgrade-Insecure-Requests': '1',
@@ -110,7 +112,7 @@ def get_detail(url, proxy):
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'accept-encoding': 'gzip, deflate, br',
         'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
-        'cookie': 'rewardsn=; wxtokenkey=777',
+        'cookie': 'rewardsn=******; wxtokenkey=******',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
     }
     try:
